b054939abac31c0f0b70d30395146db2dd94c785
MrZebarth <kris.zebarth@ocsb.ca>
2021-11-04 09:50:54 -0400

    Initial commit

---
+++ src/movietheatreproject/Movie.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package movietheatreproject;
+
+/**
+ *
+ * @author SPH_SHSM
+ */
+public class Movie {
+    String title;
+    String[] showTimes;
+    int[] ticketsLeft;
+    
+    
+    
+}
